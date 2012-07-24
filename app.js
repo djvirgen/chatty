d6b9a8f97d0f0b97bd60a896cda00da262d75758
Hector Virgen <hvirgen@attinteractive.com>
2012-07-24 13:57:26 -0700

    Changed to port 80

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = require('express').createServer()
   , io = require('socket.io').listen(app);
 
-app.listen(3000);
+app.listen(80);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/client/index.html');
